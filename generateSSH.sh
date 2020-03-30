c032f41a5977bfc50467e7acadfcf9989d14e415
caio-vinicius <sf.caio92@gmail.com>
2020-03-30 18:10:23 -0300

    script generateSSH.sh

---
+++ generateSSH.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo " - Generate SSH key..."
+mkdir -p keys
+ssh-keygen -b 2048 -t rsa -f ./keys/sshkey -N "" -q
+mv keys/sshkey.pub keys/public_key.pub && mv keys/sshkey keys/private_key
+if [[ -s keys/public_key.pub ]] && [[ -s keys/private_key ]]; then
+	echo -e "\e[32m - SSH successful created!\e[0m"
+	cp keys/public_key.pub ~/.ssh/id_rsa.pub
+	cp keys/private_key ~/.ssh/id_rsa
+	cat keys/public_key.pub | xclip -selection clipboard
+	echo -e "\e[34m - Key is already on your CTRL+V, enjoy:"
+	echo -e " - Go to profile.intra.42.fr > Settings > SSH Key > New SSH Key > CTRL + V\e[0m"
+	echo " - If it isnt, you can copy here: "
+	cat keys/public_key.pub
+	echo -e "\e[31m - NOTE: 2 keys was generated, above is the public key that locate at keys/sshkey.pub. The private key locate at keys/sshkey never share with anyone.\e[0m"
+else
+	echo -e "\e[41mSomething went wrong, I couldn't generate your SSH key."
+fi
+
+
