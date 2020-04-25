2e93dd97236cab473ce5d53906e5d424013a54fb
caio-vinicius <sf.caio92@gmail.com>
2020-04-25 15:27:09 -0300

    more readable code

--- generateSSH.sh
+++ generateSSH.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+color_message()
+{
+	echo -e "\e[$1m - $2\e[0m"
+}
+
 show_public_key()
 {
+	color_message 92 "Done"
 	ssh-keygen -y -f $1 | xclip -selection clipboard &> /dev/null
 	echo " - Key is already on your CTRL + V, enjoy."
-	echo " - If it isn't on your CTRL + V yet, you can copy here: "
+	echo " - Maybe it isn't on your CTRL + V yet, so you can copy here: "
 	echo -e "\e[1m === START KEY === \e[0m"
 	ssh-keygen -y -f $1
 	echo -e "\e[1m === END KEY === \e[0m"
@@ -15,29 +21,27 @@ generate_ssh()
 {
 	ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -N "" -q <<< y &> /dev/null
 	if [[ -s ~/.ssh/id_rsa.pub ]] && [[ -s ~/.ssh/id_rsa ]]; then
-		echo -e "\e[32m - SSH successful created!\e[0m"
 		show_public_key ~/.ssh/id_rsa
 	else
-		echo -e "\e[41mSomething went wrong, I couldn't generate your SSH key."
+		color_message 91 "Something went wrong, I couldn't generate your SSH key."
 	fi
 }
 
 choose_option()
 {
-	if [[ $1 = "yes" || $1 = "y" || $1 = "YES" || $1 = "Y" ]]; then
+	read -p " - $1 " option
+	if [[ $option = "yes" || $option = "y" || $option = "YES" || $option = "Y" ]]; then
 		$2
-	elif [[ $1 = "no" || $1 = "n" || $1 = "NO" || $1 = "N" ]]; then
+	elif [[ $option = "no" || $option = "n" || $option = "NO" || $option = "N" ]]; then
 		$3 $4
 	else
-		echo " - I can't understand what you're saying."
+		color_message 91 "I can't understand what you're saying."
 	fi
 }
 
-echo " - Working on SSH pair key..."
 if [[ -s ~/.ssh/id_rsa && -s ~/.ssh/id_rsa.pub ]]; then
-	echo " - Seems like you alredy have SSH pair keys on your system."
-	read -p " - Did you want generate a new pair? If don't I will extract your public key (yes/no): " option
-	choose_option $option generate_ssh show_public_key ~/.ssh/id_rsa
+	color_message 91 "Seems like you alredy have SSH pair keys on your system."	
+	choose_option "Did you want generate a new pair? If don't I will extract your public key (yes/no):"  generate_ssh show_public_key ~/.ssh/id_rsa
 else
 	generate_ssh
 fi
